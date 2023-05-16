a38297ded68d620db4c8b6f3d78d3f0571c6b546
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-16 10:46:27 +0300

    Add example of FileSysytem module methods

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-const { MyMath } = require("./MyMath");
-const MyComponent = require("./MyComponent");
-const { fun1, fun2, fun3, fun4, fun5 } = require("./functions");
+const fs = require("fs/promises");
+// console.log(fs);
 
-const res = MyMath.sum(3, 4);
-
-const com = new MyComponent();
-
-console.log(com.render(res));
-
-console.log(fun1());
-console.log(fun2());
-console.log(fun3());
-console.log(fun4());
-console.log(fun5());
-
+fs.readFile("./text.txt", {encoding: 'utf-8'}).then(console.log);
