f4c6cf6280e390e44d64271370f350957c83937c
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-16 11:18:56 +0300

    Add appendFile() method example

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("fs/promises");
 fs.readFile("./citadelle.txt", { encoding: "utf-8" }).then((res) => {
 	const array = res.split("\n").filter((elem) => elem !== "");
 	const randomIndex = Math.round(Math.random() * array.length - 1);
-	fs.writeFile("./quote.txt", array[randomIndex]);
+	fs.appendFile( "./append.txt", array[ randomIndex ] + '\n', 'utf-8' );
+	// fs.appendFile('./append.txt', '\n', 'utf-8')
 });
 
