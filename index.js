32f879e0f28106f448ba92031c4c99fe785b4995
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-16 11:02:09 +0300

    Add task resolve
    (readFile() task)

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const fs = require("fs/promises");
 // console.log(fs);
 
-fs.readFile("./text.txt", {encoding: 'utf-8'}).then(console.log);
+// fs.readFile( "./text.txt", { encoding: 'utf-8' } ).then( console.log );
+fs.readFile("./citadelle.txt", { encoding: "utf-8" }).then((res) => {
+	const array = res.split("\n").filter((elem) => elem !== "");
+	const randomIndex = Math.round(Math.random() * array.length);
+	console.log(array[randomIndex]);
+});
+
