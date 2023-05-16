9079172202b08359329e6b19195b0a7df4e92214
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-16 11:13:34 +0300

    Add example of writeFile() method

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const fs = require("fs/promises");
-// console.log(fs);
 
-// fs.readFile( "./text.txt", { encoding: 'utf-8' } ).then( console.log );
 fs.readFile("./citadelle.txt", { encoding: "utf-8" }).then((res) => {
 	const array = res.split("\n").filter((elem) => elem !== "");
-	const randomIndex = Math.round(Math.random() * array.length);
-	console.log(array[randomIndex]);
+	const randomIndex = Math.round(Math.random() * array.length - 1);
+	fs.writeFile("./quote.txt", array[randomIndex]);
 });
 
